d0c3b7c26eecf35cf2f1b2bfa839890636ef6097
dani <danigomezlo2022@gmail.com>
2023-02-11 14:14:41 -0500

    creandoAudio

--- portfolio/home/static/home/scripts/menu.js
+++ portfolio/home/static/home/scripts/menu.js
@@ -1,9 +1,11 @@
 // Funcion para que la musica funcione
 
+
+
 var musicIcon = document.getElementsByClassName('off')[0]
 var musicStop = document.getElementsByClassName('pause')[0]
 var audio = document.getElementById("song");
-var textoMusic = document.getElementsByClassName('moving-text')[0]
+var textoMusic = document.getElementsByClassName('text-song')[0]
 
 musicIcon.addEventListener('click', function(){
    if(audio.paused) {
