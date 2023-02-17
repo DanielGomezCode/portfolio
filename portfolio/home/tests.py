0c75f531b3ff96f78aa14e2a77c71a0bfc314572
dani <danigomezlo2022@gmail.com>
2023-02-17 13:16:41 -0500

    test

--- portfolio/home/tests.py
+++ portfolio/home/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+
+class MiTest(TestCase):
+    def test_algo(self):
+        pass
+    
+def test_suma(self):
+    self.assertEqual(2+2, 4)
